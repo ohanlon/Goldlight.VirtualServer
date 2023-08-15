d0c737430c6fe73a502947d049ff5c6b91e056bd
Pete O'Hanlon <pete.ohanlon@gmail.com>
2023-08-15 16:05:47 +0100

    Add project files.

---
+++ Goldlight.VirtualServer/Program.cs
@@ -0,0 +1,56 @@
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+  var forecast = Enumerable.Range(1, 5).Select(index =>
+      new WeatherForecast
+      (
+          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+          Random.Shared.Next(-20, 55),
+          summaries[Random.Shared.Next(summaries.Length)]
+      ))
+      .ToArray();
+  return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run(async context => { await WriteResponse(context,"Hello ServiceVirtualization"); });
+
+app.Run();
+
+static async Task WriteResponse<T>(HttpContext context, T result, int statusCode = 200, string contentType = "application/json")
+{
+  HttpResponse response = context.Response;
+  response.StatusCode = statusCode;
+  response.ContentType = contentType;
+  await response.WriteAsync(JsonSerializer.Serialize(result));
+}
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
